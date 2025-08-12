fdca2e641e9b77c6406be8046360b6608b2b1d0d
LucasPandian <lucaspandian2017@gmail.com>
2025-08-12 14:08:10 -0300

    commit 1

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,12 +1,20 @@
 package application.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Arrays;
+import java.util.List;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class IndexController {
-    @RequestMapping("/")
-    public String home() {
-        return "home";
-    }
+    // Criando um conjunto de dados
+    private List<String> frutas = Arrays.asList(...a:"Maçã", "Uva", "Banana");
+
+    @GetMapping
+    public List<String> home() {
+        return frutas;
+    }
 }
