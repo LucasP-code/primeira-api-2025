3f04ae0e7f3eabd64cdcbe5585f39ffe0798220e
Lucas P. Pereira <lucaspandian2017@gmail.com>
2025-08-12 19:08:34 +0000

    Aula 12/08/2025

--- src/main/java/application/controller/IndexController.java
+++ src/main/java/application/controller/IndexController.java
@@ -1,9 +1,12 @@
 package application.controller;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class IndexController {
     // Criando um conjunto de dados
-    private List<String> frutas = Arrays.asList(...a:"Maçã", "Uva", "Banana");
+    //private List<String> frutas = Arrays.asList("Maçã", "Uva", "Banana");
+    private List<String> frutas = new ArrayList<String>();
 
     @GetMapping
     public List<String> home() {
-        return frutas;
-    }
+        return frutas;
+    }
+
+    @PostMapping
+    public List<String> insert(@RequestBody String fruta) {
+        frutas.add(fruta);
+        return frutas;
+    }
 }
